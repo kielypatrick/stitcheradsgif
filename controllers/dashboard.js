bce32b3ade2bc87432540365cbe80dab56dba5bc
Glitch (necessary-opinion) <none>
2018-02-15 12:54:14 +0000

    💉📖 Checkpoint models/picture-store.js:344484/14 controllers/dashboard.js:344484/12

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,15 +3,20 @@
 const logger = require('../utils/logger');
 const accounts = require('./accounts.js');
 const pictureStore = require('../models/picture-store.js');
+const cloudinary = require('cloudinary');
+
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
+    cloudinary.v2.uploader.multi('bitmoji',
+    function(error,result) {console.log(result) });
     const loggedInUser = accounts.getCurrentUser(request);
     const viewData = {
       title: 'PictureStore Dashboard',
       user: loggedInUser,
       album: pictureStore.getAlbum(loggedInUser.id),
+
     };
     response.render('dashboard', viewData);
   },
