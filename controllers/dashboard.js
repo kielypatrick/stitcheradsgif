98ff8f002c09a323d0487ceac870f49bb429349b
Glitch (necessary-opinion) <none>
2018-02-16 13:14:14 +0000

    🐣🌪 Checkpoint controllers/dashboard.js:344484/62

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -44,8 +44,7 @@ const dashboard = {
   createGif(request, response) {
     const loggedInUser = accounts.getCurrentUser(request);
     cloudinary.v2.uploader.multi(request.body.tag,
-   function(error,result) {console.log(result) }
-                                delay: 1000);
+   function(error,result) {console.log(result) });
       response.redirect('/dashboard');
       logger.info('creating gif:' + request.body.tag)
 
